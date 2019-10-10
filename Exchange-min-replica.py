9d3203c1e7e00cc14545c10ecd53b7d02a3c1b65
Derek Shore <des2037@med.cornell.edu>
2019-10-10 16:13:11 -0400

    code now uses definitions.py

--- Exchange-min-replica.py
+++ Exchange-min-replica.py
@@ -1,6 +1,7 @@
 
 from imports import *
 from fep_functions import (_get_pme_direct_space_unique_expression, _get_electrostatics_energy_expressions, add_global_parameters, calc_system_charge, create_force_particle, create_force_bond)
+from definitions import *
 
 print('Building system...')
 psf = CharmmPsfFile('./inputFiles/fep-40-i01-1-wb-i.psf')
@@ -60,98 +61,6 @@ glu_side_atoms = top.select('resname GLU2 and name CG CD OE1 OE2')
 side_atoms = np.concatenate((lys_side_atoms, his_side_atoms, glu_side_atoms))
 side_atoms = np.sort(side_atoms)
 
-# Define VdW softcore parameters
-switch_width=1*unit.angstroms
-softcore_alpha = 0.5
-softcore_a = 1
-softcore_b = 1
-softcore_c = 1
-softcore_beta = 0.0
-softcore_d = 1
-softcore_e = 1
-softcore_f = 1
-# Define the replica exchange procedure
-pH_low = 2
-pH_high = 12
-pH_step = 0.5
-steps = 100
-# Default pKa values
-EpKa = 4.4
-KpKa = 10.4
-HpKa = 6.5
-HpKa_switch = 9.1
-CpKa = 9.5
-DpKa = 4.0
-#Lambda lists
-l_Glu = {}
-l_His = {}
-l_Lys = {}
-l_Cys = {}
-l_Asp = {}
-l_Glu_special = {}
-glu_char = {}
-lys_char = {}
-his_char_E = {}
-his_char_D = {}
-asp_char = {}
-cys_char = {}
-# GLU difference charges
-glu_char["CG:"] = 0.07
-glu_char["CD:"] = 0.13
-glu_char["OE1:"] = 0.21
-glu_char["OE2:"] = 0.15
-# LYS difference charges
-lys_char["CE:"] = 0.08
-lys_char["HE1:"] = -0.025
-lys_char["HE2:"] = -0.025
-lys_char["NZ:"] = 0.66
-lys_char["HZ1:"] = -0.01
-lys_char["HZ2:"] = -0.01
-# HSE difference charges
-his_char_E["CB:"] = 0.03
-his_char_E["ND1:"] = 0.19
-#his_char_E["HD1:"] = 0.44
-his_char_E["CG:"] = -0.03
-his_char_E["CE1:"] = 0.07
-his_char_E["HE1:"] = 0.05
-his_char_E["NE2:"] = -0.15
-his_char_E["HE2:"] = 0.12
-his_char_E["CD2:"] = 0.24
-his_char_E["HD2:"] = 0.04
-# HSD difference charges
-his_char_D["CB:"] = 0.04
-his_char_D["ND1:"] = -0.15
-his_char_D["HD1:"] = 0.12
-his_char_D["CG:"] = 0.24
-his_char_D["CE1:"] = 0.07
-his_char_D["HE1:"] = 0.05
-his_char_D["NE2:"] = 0.19
-#his_char_D["HE2:"] = 0.44
-his_char_D["CD2:"] = -0.03
-his_char_D["HD2:"] = 0.03
-# CYS difference charges
-cys_char["CB:"] = 0.27
-cys_char["SG:"] = 0.57
-# ASP difference charges
-asp_char["CB:"] = 0.07
-asp_char["CG:"] = 0.13
-asp_char["OD1:"] = 0.21
-asp_char["OD2:"] = 0.15
-
-for i in range(pH_low*10, pH_high*10, 5):
-    p = 1-1/(1+10**(EpKa-i/10))
-    l_Glu[i/10] = '%.4f'%p
-    p = 1-1/(1+10**(HpKa-i/10))
-    l_His[i/10] = '%.4f'%p
-    p = 1-1/(1+10**(KpKa-i/10))
-    l_Lys[i/10] = '%.4f'%p
-    p = 1-1/(1+10**(CpKa-i/10))
-    l_Cys[i/10] = '%.4f'%p
-    p = 1-1/(1+10**(DpKa-i/10))
-    l_Asp[i/10] = '%.4f'%p
-pi = round(pi, 5)
-
-pH_list = np.arange(pH_low, pH_high, 0.5)
 print(pH_list)
 all_atoms_HSE = np.concatenate((lys_atoms, his_atoms_E, glu_atoms))
 all_atoms_HSE = np.sort(all_atoms_HSE)

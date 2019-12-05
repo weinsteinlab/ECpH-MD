479c9172252be94b659eed8d9e397c794f4aa4ad
Ekaterina Kots <edk4002@node003.panda>
2019-12-05 16:43:58 -0500

    added the psf making scripts

---
+++ psf_script/ionize.tcl
@@ -0,0 +1,9 @@
+ package require autoionize
+ topology top_all36_prot.rtf
+ topology toppar_water_ions.str
+ autoionize -psf hewl-wb.psf -pdb hewl-wb.pdb -sc 0.15 -o hewl-wb-i
+ #autoionize -psf 59-wb.psf -pdb 59-wb.pdb -neutralize -o 59-wb-i
+ #autoionize -psf 63-wb.psf -pdb 63-wb.pdb -neutralize -o 63-wb-i
+ #autoionize -psf 71-wb.psf -pdb 71-wb.pdb -neutralize -o 71-wb-i
+ #autoionize -psf 80-wb.psf -pdb 80-wb.pdb -neutralize -o 80-wb-i
+ #autoionize -psf 88-wb.psf -pdb 88-wb.pdb -neutralize -o 88-wb-i
---
+++ psf_script/solvate.tcl
@@ -0,0 +1,5 @@
+ package require solvate 	 
+ topology top_all36_prot.rtf
+ topology toppar_water_ions.str
+ solvate hewl-crw.psf hewl-crw.pdb -t 35 -o hewl-wb
+ 

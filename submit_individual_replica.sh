1046d8a3736138703dd2c6442263c70a982def72
des2037 <des2037@med.cornell.edu>
2020-11-25 15:59:41 -0500

    fixes replica job submission options for rpi

--- submit_individual_replica.sh
+++ submit_individual_replica.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #SBATCH --job-name=bb6
-#SBATCH -p edison
+#SBATCH -p dcs
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=10G
+#SBATCH --mem=40G
 ##SBATCH --mail-user=CWID@med.cornell.edu
 ##SBATCH --mail-type=END
 
-source /home/des2037/.bashrc
+source ~/.bashrc
 
-spack load -r /62q4vgx # this is cuda@9.2.88
-#spack load -r cuda@9.2.88
-spack load -r /omvpd5u # fftw@3.3.8
+module load gcc/8.1.0/1
+module load cuda/10.1
 
-conda activate pHreplicaExchange 
+conda activate openmm_7.4.0
+
 
 pH=$1
 #iteration=$2

1cb50dd91d09a5183bc503f28de016a98ac9d64c
des2037 <des2037@med.cornell.edu>
2020-12-07 16:56:40 -0500

    adds exchange condition scaffold

--- submit_job.sh
+++ submit_job.sh
@@ -14,16 +14,21 @@ swarmNumber_padded=`printf %04d $swarmNumber`
 
 mkdir -p energies propagate_runs simulations submission_logs lambdas 
 
-for (( subjob=1; subjob<=$number_of_subjobs; subjob++ ))
-do
-  if [ $first_subjob -eq 0 ]; then
-    jobSchedulerOutput="$(sbatch -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replicas})"
-  else
-    jobSchedulerOutput="$(sbatch --depend=afterok:${job_scheduler_number} -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replias})"
-  fi
+for (( subjob=1; subjob<=$number_of_subjobs; subjob++ )); do
+    if [ $first_subjob -eq 0 ]; then
+        jobSchedulerOutput="$(sbatch -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replicas})"
 
-  job_scheduler_number=$(echo $jobSchedulerOutput | awk '{print $2}' | sed -e 's/<//' | sed -e 's/>//')
-  let first_subjob=1
+    else
+        if [ $subjobs_before_exchange != 0 ] && [ $(($subjob % $subjobs_before_exchange)) == 0 ]; then
+            echo "exchange time!" 
+            # insert job submission for exchange
+        else 
+            jobSchedulerOutput="$(sbatch --depend=afterok:${job_scheduler_number} -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replias})"
+        fi
+    fi
+
+    job_scheduler_number=$(echo $jobSchedulerOutput | awk '{print $2}' | sed -e 's/<//' | sed -e 's/>//')
+    let first_subjob=1
 done
 
 exit

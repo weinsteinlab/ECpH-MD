3a69a74f40d677bace1284dbbd5d73005fd3dfb8
des2037 <des2037@med.cornell.edu>
2020-12-07 11:20:43 -0500

    main submission script ready for RPI

--- submit_job.sh
+++ submit_job.sh
@@ -5,6 +5,7 @@ subjobs_per_iteration=1
 iterations_per_subjob=2
 number_of_subjobs=1 
 jobName="dat" # no spaces
+partitionName=dcs            #Slurm partition to run job on
 
 # do not edit below this line
 
@@ -21,9 +22,9 @@ mkdir -p erf_files energies progress propagate_runs simulations submission_logs 
 for (( subjob=1; subjob<=$number_of_subjobs; subjob++ ))
 do
   if [ $first_subjob -eq 0 ]; then
-    jobSchedulerOutput="$(bsub -P BIP109 -W 2:00 -nnodes $numberOfNodes -J ./submission_logs/${jobName} -alloc_flags 'gpumps smt4' ./submit_Exchange-min-replica.sh ${number_of_replicas} ${subjobs_per_iteration} ${iterations_per_subjob})"
+    jobSchedulerOutput="$(sbatch -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replicas} ${subjobs_per_iteration} ${iterations_per_subjob})"
   else
-    jobSchedulerOutput="$(bsub -P BIP109 -W 2:00 -nnodes $numberOfNodes -J ./submission_logs/${jobName} -alloc_flags 'gpumps smt4' -w $job_scheduler_number ./submit_Exchange-min-replica.sh ${number_of_replicas} ${subjobs_per_iteration} ${iterations_per_subjob})"
+    jobSchedulerOutput="$(sbatch --depend=afterok:${job_scheduler_number} -J ./submission_logs/${jobName} -N ${numberOfNodes} -p $partitionName --gres=gpu:32g:6 -C cuda-mode-exclusive -t 0-02:00:00 ./submit_Exchange-min-replica.sh ${number_of_replicas} ${subjobs_per_iteration} ${iterations_per_subjob})"
   fi
 
   job_scheduler_number=$(echo $jobSchedulerOutput | awk '{print $2}' | sed -e 's/<//' | sed -e 's/>//')

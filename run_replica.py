d7f679b296998641b46bc537a734276ddd0edc41
des2037 <des2037@med.cornell.edu>
2020-12-07 19:25:07 -0500

    fixes directory nomenclature

--- run_replica.py
+++ run_replica.py
@@ -55,9 +55,9 @@ except NameError:
 
 print(' pH:', pH, ' subjob:', subjob_number, 'replica:', replica_number)
 
-output_directory = './simulations/pH_' + str(pH) + '_replica_number_' + str(replica_number).zfill(4)
-output_name = str(output_name) + '-ph' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '-subjob' + str(subjob_number).zfill(4) 
-full_output_name = output_directory + str(output_name) + '-ph' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '-subjob' + str(subjob_number).zfill(4)  
+output_directory = './simulations/pH_' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '/'
+output_no_path = str(output_name) + '-ph' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '-subjob' + str(subjob_number).zfill(4) 
+full_output_name = output_directory + str(output_no_name)
 
 if subjob_number == 0:
     print("Reading in previous state: FALSE")
@@ -80,16 +80,16 @@ if subjob_number == 0:
     state = simulation.context.getState(getPositions=False, getVelocities=False, getForces=False, getEnergy=True, getParameters=False, getParameterDerivatives=False)
     energy = state.getKineticEnergy()._value + state.getPotentialEnergy()._value
     energy = pd.DataFrame(np.array([energy]))
-    energy.to_csv('./energies/' + str(output_name) + '-energy.csv')
+    energy.to_csv('./energies/' + str(output_no_path) + '-energy.csv')
     simulation.minimizeEnergy(maxIterations=1000)
     state_min = simulation.context.getState(getPositions=False, getVelocities=False, getForces=False, getEnergy=True, getParameters=False, getParameterDerivatives=False)
     energy_min = state_min.getKineticEnergy()._value + state_min.getPotentialEnergy()._value
     energy_min = pd.DataFrame(np.array([energy_min]))
-    energy_min.to_csv('./energies/' + str(output_name) + '-energy-min.csv')
+    energy_min.to_csv('./energies/' + str(output_no_path) + '-energy-min.csv')
     print('FINISH')
 
 else:
-    previous_output_name=output_directory + str(output_name) + '-ph' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '-subjob' + str(subjob_number - 1).zfill(4)
+    previous_output_name=output_directory + str(output_no_path) + '-ph' + str(pH) + '_replica_number_' + str(replica_number).zfill(4) + '-subjob' + str(subjob_number - 1).zfill(4)
     print("Reading previous state from: " + previous_output_name + "-state.xml")
     simulation.loadState(previous_output_name + '-state.xml')
     positions = simulation.context.getState(getPositions=True).getPositions()
@@ -106,22 +106,22 @@ else:
 
     state = simulation.context.getState(getPositions=False, getVelocities=False, getForces=False, getEnergy=True, getParameters=False, getParameterDerivatives=False)
     energy = state.getKineticEnergy()._value + state.getPotentialEnergy()._value
-    energy_input = pd.read_csv(('./energies/' + str(output_name) + '-energy.csv'), index_col=0)
+    energy_input = pd.read_csv(('./energies/' + str(output_no_path) + '-energy.csv'), index_col=0)
     energy_last = pd.DataFrame(np.array([energy]))
     name_i = str(energy_last.columns.tolist()[0])
     name_j = str(energy_input.shape[1])
     energy_current = energy_last.rename(columns={name_i: name_j})
     output = pd.concat([energy_input, energy_current], axis=1, sort=False)
-    output.to_csv('./energies/' + str(output_name) + '-energy.csv')
+    output.to_csv('./energies/' + str(output_no_path) + '-energy.csv')
     simulation.minimizeEnergy(maxIterations=1000)
     state_min = simulation.context.getState(getPositions=False, getVelocities=False, getForces=False, getEnergy=True, getParameters=False, getParameterDerivatives=False)
     energy_min = state_min.getKineticEnergy()._value + state_min.getPotentialEnergy()._value
-    energy_min_input = pd.read_csv(('./energies/' + str(output_name) + '-energy-min.csv'), index_col=0)
+    energy_min_input = pd.read_csv(('./energies/' + str(output_no_path) + '-energy-min.csv'), index_col=0)
     energy_min_last = pd.DataFrame(np.array([energy_min]))
     name_i = str(energy_min_last.columns.tolist()[0])
     name_j = str(energy_min_input.shape[1])
     energy_min_current = energy_min_last.rename(columns={name_i: name_j})
     output = pd.concat([energy_min_input, energy_min_current], axis=1, sort=False)
-    output.to_csv('./energies/' + str(output_name) + '-energy-min.csv')
+    output.to_csv('./energies/' + str(output_no_path) + '-energy-min.csv')
     print('FINISH')
 
